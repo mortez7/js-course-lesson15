4074100fe355d93bdb4040cd5388c207a4d0a0c0
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2025-01-01 12:21:54 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const DomElement = function (selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+
+  this.createElement = function () {
+    let newElement = "";
+    if (this.selector.startsWith(".")) {
+      newElement = document.createElement("div");
+    } else if (this.selector.startsWith("#")) {
+      newElement = document.createElement("p");
+    }
+
+    newElement.classList.add(this.selector);
+    newElement.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
+    newElement.textContent = "asdifsudhusduhfusd";
+    document.body.append(newElement);
+  };
+};
+
+const newBlock = new DomElement(".block", "100px", "200px", "yellow", "14px");
+const newText = new DomElement("#best", "300px", "400px", "green", "18px");
+
+newBlock.createElement();
+newText.createElement();
