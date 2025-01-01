24e7e9b1fe426f831a76cccb4fb51a24abdbd54d
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2025-01-01 13:17:06 +0200

    fix: восстановлен вырезанный код

--- js/main.js
+++ js/main.js
@@ -7,37 +7,43 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   this.bg = bg;
   this.fontSize = fontSize;
 
-  (this.createElement = function () {
+  this.createElement = function () {
     let newElement = "";
-
+    if (this.selector.startsWith(".")) {
+      newElement = document.createElement("div");
+      newElement.classList.add(this.selector.slice(1));
+    } else if (this.selector.startsWith("#")) {
+      newElement = document.createElement("p");
+      newElement.id = this.selector.slice(1);
+    }
     this.position = { top: 100, left: 100 };
     newElement.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}; position: absolute; top: ${this.position.top}px; left: ${this.position.left}px;`;
 
     document.body.append(newElement);
     this.element = newElement;
-  }),
-    (this.move = function (event) {
-      const step = 10;
-      switch (event.key) {
-        case "ArrowUp":
-          this.position.top -= step;
-          break;
-        case "ArrowDown":
-          this.position.top += step;
-          break;
-        case "ArrowLeft":
-          this.position.left -= step;
-          break;
-        case "ArrowRight":
-          this.position.left += step;
-          break;
-        default:
-          return;
-      }
+  };
+  this.move = function (event) {
+    const step = 10;
+    switch (event.key) {
+      case "ArrowUp":
+        this.position.top -= step;
+        break;
+      case "ArrowDown":
+        this.position.top += step;
+        break;
+      case "ArrowLeft":
+        this.position.left -= step;
+        break;
+      case "ArrowRight":
+        this.position.left += step;
+        break;
+      default:
+        return;
+    }
 
-      this.element.style.top = `${this.position.top}px`;
-      this.element.style.left = `${this.position.left}px`;
-    });
+    this.element.style.top = `${this.position.top}px`;
+    this.element.style.left = `${this.position.left}px`;
+  };
 };
 
 const newSquare = new DomElement(".block", "100px", "100px", "yellow", "14px");
